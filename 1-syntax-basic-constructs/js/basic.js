391e07ceed7deb6706c931150d382d0414b29b04
AndresR9707 <afrivera@jdc.edu.co>
2021-02-02 07:16:09 -0500

    exercise basic javascript

---
+++ 1-syntax-basic-constructs/js/basic.js
@@ -0,0 +1,12 @@
+function ValidAge(){
+const question = prompt("How old are you?"); 
+if(question <= 17){
+    alert('You are not an adult.')
+}else{
+    alert('You are an adult.')
+    }
+}
+
+
+
+
